5e8aee6fe0c883d609f8a788b65fb5e4206e1d17
Lebroncorp <lebroncorp@yahoo.com>
2023-09-05 14:47:06 -0700

    print combo perfevt

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for a in range(10):
+for a in range(8):
     for b in range(a + 1, 10):
         if b == a:
             continue
         print("{:d}{:d}".format(a, b), end=', ')
-print()
+print("{:d}{:d}".format(a + 1, b))
