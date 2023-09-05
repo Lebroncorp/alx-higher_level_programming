85acbfd32980ecffbaeba18f6eb7ef78edeecceb
Lebroncorp <lebroncorp@yahoo.com>
2023-09-05 13:46:05 -0700

    skip alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for char in range(97, 122):
-    if char == 101 or char ==113:
+for char in range(97, 123):
+    if char == 101 or char == 113:
         continue
     else:
         print("{:c}".format(char), end='')
