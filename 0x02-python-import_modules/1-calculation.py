a158675e2a8c7627f585313969e72c3a03599c9d
Lebroncorp <lebroncorp@yahoo.com>
2023-09-07 16:16:05 -0700

    d

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,6 +2,8 @@
 
 from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+
     a = 10
     b = 5
 
@@ -10,7 +12,7 @@ from calculator_1 import add, sub, mul, div
     mula = mul(a, b)
     diva = div(a, b)
 
-print("{} + {} = {}".format(a, b, adda))
-print("{} + {} = {}".format(a, b, suba))
-print("{} + {} = {}".format(a, b, mula))
-print("{} + {} = {}".format(a, b, diva))
+print(f"{a} + {b} = {adda}")
+print(f"{a} + {b} = {suba}")
+print(f"{a} + {b} = {mula}")
+print(f"{a} + {b} = {diva}")
