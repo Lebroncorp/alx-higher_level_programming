54302e481874b9d362cccfdd2bd3e11d5a204548
Lebroncorp <lebroncorp@yahoo.com>
2023-09-07 16:24:46 -0700

    d

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    result = len(sys.argv) - 1
+
+    if result == 0:
+        print("0 arguments.")
+    elif result == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(result))
+    for i in range(result):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
