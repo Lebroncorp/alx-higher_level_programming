883bf11de3aeee10498c6c44153ecc6cacbb0c2d
Lebroncorp <lebroncorp@yahoo.com>
2023-09-11 07:17:33 -0700

    complete

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     divisible = []
-    for i in my_list:
+    for i in len(my_list):
         if my_list[i] % 2 == 0:
             divisible.append(True)
         else:
