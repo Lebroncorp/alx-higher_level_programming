ea62f70ee64b903055e4f4d8cca151dddd27a057
Lebroncorp <lebroncorp@yahoo.com>
2023-09-15 09:20:40 -0700

    som

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-def multiply_list_map(my_list=[], number=0):
+def best_score(a_dictionary):
+    if (a_dictionary is None or a_dictionary == {}):
+        return None
+
+    sort_dict = sorted(a_dictionary)
+    max_value = sort_dict.pop()
+    return (max_value)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if (type(roman_string) is not string and roman_string is None):
+        return 0
