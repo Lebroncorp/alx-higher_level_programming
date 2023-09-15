0ab01744d553ec50b98296417eeea58c32b882af
Lebroncorp <lebroncorp@yahoo.com>
2023-09-15 09:02:29 -0700

    kd

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return ({b: a_dictionary[b] * 2 for b in a_dictionary})
+    new_dictionary = {}
+
+    for i, j in a_dictionary.items():
+        new_dictionary[i] = j * 2
+
+    return new_dictionary
