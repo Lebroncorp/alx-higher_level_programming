f4fa5672b07f71da9ae83c6367c0db1b0ec84bdf
Lebroncorp <lebroncorp@yahoo.com>
2023-09-15 08:22:23 -0700

    sd

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,12 @@
 
 def uniq_add(my_list=[]):
     adds = 0
-    for i in set(my_list):
+    new_list = []
+
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+
+    for i in new_list:
         adds += i
     return adds
