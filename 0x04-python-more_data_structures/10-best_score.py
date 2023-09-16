7df0184a2dd0cde8f59e13b2a5d6f7b7c385a623
Lebroncorp <lebroncorp@yahoo.com>
2023-09-15 19:12:21 -0700

    dsa

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if (a_dictionary is None or a_dictionary == {}):
+    if not a_dictionary:
         return None
+    return max(a_dictionary, key=a_dictionary.get)
 
-    sort_dict = sorted(a_dictionary)
-    max_value = sort_dict.pop()
-    return (max_value)
+#    if (a_dictionary is None or a_dictionary == {}):
+#        return None
+
+#    sort_dict = sorted(a_dictionary)
+#    max_value = sort_dict.pop()
+#    return (max_value)
