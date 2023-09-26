45b06995481b883c397b4c31d7814ad5ebe8920d
Lebroncorp <lebroncorp@yahoo.com>
2023-09-25 19:20:07 -0700

    magic calculation

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+    resolve = None
+    try:
+        resolve = fct(*args)
+
+    except Exception as errors:
+        errors = errors.args[0]
+        sys.stderr.write("Exception: " + errors + "\n")
+
+    return (resolve) 
