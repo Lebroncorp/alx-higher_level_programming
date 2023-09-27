b75222b0aadb116447b7b6599913107af9bb2d05
Lebroncorp <lebroncorp@yahoo.com>
2023-09-26 18:33:22 -0700

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a private attribute size square."""
+
+    def __init__(self, size):
+        """Initialize function of a Square size."""
+        self.__size = size
