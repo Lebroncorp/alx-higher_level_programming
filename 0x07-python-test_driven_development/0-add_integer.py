278a295fffe6c0165e686adb9d0bb2b03af755e8
Lebroncorp <lebroncorp@yahoo.com>
2023-10-03 09:25:46 -0700

    add and divide

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,11 +7,16 @@
 def add_integer(a, b):
     """Return the addition of two numbers."""
 
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
+    if type(b) == ():
+        b = 0
+    if type(a) == ():
+        a = 0
 
     if type(b) is float:
         b = int(b)
