fb420a33ea724ca7f7a0c08e98bb2daf0a696f83
Lebroncorp <lebroncorp@yahoo.com>
2023-10-03 07:47:09 -0700

    add and divide

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""defines function to scalar divde matrix"""
+"""
+defines function to scalar divde matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    """divides matrix by scalar integer, approximated to two decimal places"""
-
+    """divides matrix by scalar integer, rounded to two decimal places"""
     import decimal
     error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
     if type(matrix) is not list:
         raise TypeError(error_msg)
+
     row_lens = []
     count_rows = 0
 
@@ -21,6 +24,7 @@ def matrix_divided(matrix, div):
             if type(element) not in [int, float]:
                 raise TypeError(error_msg)
         count_rows += 1
+
     if len(set(row_lens)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
 
@@ -29,6 +33,7 @@ def matrix_divided(matrix, div):
 
     if int(div) == 0:
         raise ZeroDivisionError("division by zero")
+
     new_matrix = list(map(lambda row:
                           list(map(lambda x: round(x/div, 2), row)), matrix))
     return new_matrix
