db179be81019605a4facf8cfbbf99b55f4247a18
Lebroncorp <lebroncorp@yahoo.com>
2023-10-10 17:18:20 -0700

    square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Contains the class Square and subclass Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """An instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns area of the square"""
+        return self.__size ** 2
