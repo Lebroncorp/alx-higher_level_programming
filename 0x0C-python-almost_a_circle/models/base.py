0476f296513dbd569a40ce44ae4b65ba71e22097
Lebroncorp <lebroncorp@yahoo.com>
2023-10-15 17:39:19 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+"""
+    Defines a Base class module
+"""
 
-def __init__(self, id=None):
+
+class Base:
+    """ Base for all other class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializes the base class with its attributes id(int) """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
