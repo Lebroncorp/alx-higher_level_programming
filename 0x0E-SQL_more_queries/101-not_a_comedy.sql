0d59121f2f6e5a0f9dc33ab9f3c6cc6799feeefe
Lebroncorp <lebroncorp@yahoo.com>
2023-11-16 18:22:49 -0800

    tv shows genres

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -10,10 +10,10 @@ ON g.`id` = sg.`genre_id`
 WHERE t.`title` NOT IN
 (SELECT `title`
 	FROM `tv_shows` AS t
-	INNER JOIN `tv_show_genres` AS s
-	ON s.`show_id` = t.`id`
+	INNER JOIN `tv_show_genres` AS sg
+	ON sg.`show_id` = t.`id`
 	
 	INNER JOIN `tv_genres` AS g
-	ON g.`id` = s.`genre_id`
+	ON g.`id` = sg.`genre_id`
 	WHERE g.`name` = "Comedy")
 ORDER BY `title`;
