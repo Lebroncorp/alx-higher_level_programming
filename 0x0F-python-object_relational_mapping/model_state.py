81e46ee992d2d94d8f67cf56d1b30df9e7221694
Lebroncorp <lebroncorp@yahoo.com>
2023-12-17 19:28:05 -0800

    store states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/pyhton3
+
+"""
+Defines a State model.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state.
+    __tablename__ (str): The name of the MySQL table to store the States.
+    id: The state's id.
+    name: The state's name.
+    """
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
