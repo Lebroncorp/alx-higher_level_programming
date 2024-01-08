bf73eda70cb9a55018ea5bd3f7b049372902e6cd
Lebron <lebroncorp@yahoo.com>
2024-01-07 16:59:09 -0800

    requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""ends a request to the URL
+displays the value of the variable X-Request-Id in the response header"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
