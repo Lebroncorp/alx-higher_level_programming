c9f0d782a647066a9d67b7c24cf906ab37bfc3f2
Lebroncorp <lebroncorp@yahoo.com>
2024-01-30 15:36:56 -0800

    infile

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+//computes the number of tasks completed by user id.
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-readme.js <url>');
+  process.exit(1);
+}
+const url = process.argv[2];
+
+
+// display request's status code
+request.get(`${url}`, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  const completed = {};
+  const data = JSON.parse(body);
+
+  data.forEach((task) => {
+    if (task.completed) {
+      if (!completed[task.userId]) {
+        completed[task.userId] = 1;
+      } else {
+        completed[task.userId]++;
+      }
+    }
+  });
+  console.log(completed);
+});
