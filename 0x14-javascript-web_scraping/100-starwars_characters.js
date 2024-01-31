31c619b7fc84613279b68f11af1feb681f1ffba6
Lebroncorp <lebroncorp@yahoo.com>
2024-01-30 20:10:12 -0800

    starwars

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request.get(url + id, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const character = data.characters;
+  for (const i of character) {
+    request.get(i, function (error, response, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
