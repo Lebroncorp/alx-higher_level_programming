07c0b1f87554450bd761d638078fd2533d81b261
Lebroncorp <lebroncorp@yahoo.com>
2024-01-30 21:25:04 -0800

    add new string

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,13 +3,13 @@
 const fs = require('fs');
 
 if (process.argv.length !== 4) {
-  console.error('Usage: ./0-readme.js <file-path> <content>');
+  console.error('Usage: ./0-readme.js <file-path> <newstr>');
   process.exit(1);
 }
 const filePath = process.argv[2];
-const content = process.argv[3];
+const newstr = process.argv[3];
 
-fs.writeFile(filePath, content, err => {
+fs.writeFile(filePath, newstr, err => {
   if (err) {
     console.error(err);
   }
